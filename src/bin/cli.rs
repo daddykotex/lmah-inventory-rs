0ec5131ead3a27076e88a15d09077e7b34e747b2
David Francoeur <dfrancoeur04@gmail.com>
2026-03-17 16:20:21 -0400

    feat: add args for retrieving src and target and check them

--- src/bin/cli.rs
+++ src/bin/cli.rs
@@ -1,4 +1,5 @@
 use clap::Parser;
+use std::{fs, path::PathBuf};
 
 /// CLI tool to load the data from a JSON database into a SQL database
 #[derive(Parser, Debug)]
@@ -6,10 +7,32 @@ use clap::Parser;
 struct Args {
     /// Location of the JSON file
     #[arg(short, long)]
-    location: String,
+    src: PathBuf,
+
+    
+    /// Location of the SQLite database
+    #[arg(short, long)]
+    target: PathBuf,
+}
+
+fn assert_args(args: &Args) {
+    assert!(args.src.exists(), "The source JSON file does not exist.");
+    assert!(
+        args.src.extension().expect("No extension on the source JSON file.") == "json",
+        "The extension is not `.json`."
+    );
+
+    assert!(args.target.exists(), "The target SQLite database file does not exist.");
+    assert!(
+        args.target.extension().expect("No extension on the target file.") == "db",
+        "The extension is not `.db`."
+    );
 }
 
 fn main() {
     let args = Args::parse();
-    println!("Hello {}!", args.location);
+
+    assert_args(&args);
+
+    println!("Hello {}!", args.src.display());
 }
