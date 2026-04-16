12275a9263bbc06b228b38c9f379a34684f549ee
David Francoeur <dfrancoeur04@gmail.com>
2026-04-16 08:16:58 -0400

    fix: redirect to factures on /

--- src/server/routes/bootstrap.rs
+++ src/server/routes/bootstrap.rs
@@ -1,5 +1,5 @@
 use anyhow::Result;
-use axum::Router;
+use axum::{Router, response::Redirect, routing::get};
 use sqlx::SqlitePool;
 use tower_http::services::ServeDir;
 
@@ -7,10 +7,15 @@ use crate::server::routes::{
     clients::client_router, events::event_router, factures::facture_router,
 };
 
+async fn redirect_to_factures() -> Redirect {
+    Redirect::to("/factures")
+}
+
 pub async fn setup_routes() -> Result<Router<SqlitePool>> {
     Ok(Router::new()
         .merge(client_router())
         .merge(event_router())
         .merge(facture_router())
+        .route("/", get(redirect_to_factures))
         .nest_service("/static", ServeDir::new("static")))
 }
