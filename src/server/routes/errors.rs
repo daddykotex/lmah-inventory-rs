6875db6cbde807d027952c134478179832e2129d
David Francoeur <dfrancoeur04@gmail.com>
2026-05-04 14:52:11 -0400

    fix: log outgoing http errors

--- src/server/routes/errors.rs
+++ src/server/routes/errors.rs
@@ -8,6 +8,7 @@ pub struct AppError(anyhow::Error);
 
 impl IntoResponse for AppError {
     fn into_response(self) -> Response {
+        eprintln!("Returning error at the HTTP layer: {:?}", self.0);
         (
             StatusCode::INTERNAL_SERVER_ERROR,
             format!("Something went wrong: {}", self.0),
